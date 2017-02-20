fee98014dd106d2fbccb9d65efc99a21f00db291
YinjianChen <YinjianChen02@gmail.com>
2017-02-20 22:43:09 +0800

    创建内容扩展

---
+++ YTContent/NotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.h
+//  YTContent
+//
+//  Created by Decade on 2017/2/20.
+//  Copyright © 2017年 YinjianChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
---
+++ YTContent/NotificationViewController.m
@@ -0,0 +1,30 @@
+//
+//  NotificationViewController.m
+//  YTContent
+//
+//  Created by Decade on 2017/2/20.
+//  Copyright © 2017年 YinjianChen. All rights reserved.
+//
+
+#import "NotificationViewController.h"
+#import <UserNotifications/UserNotifications.h>
+#import <UserNotificationsUI/UserNotificationsUI.h>
+
+@interface NotificationViewController () <UNNotificationContentExtension>
+
+@property IBOutlet UILabel *label;
+
+@end
+
+@implementation NotificationViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any required interface initialization here.
+}
+
+- (void)didReceiveNotification:(UNNotification *)notification {
+    self.label.text = notification.request.content.body;
+}
+
+@end
